19373f165c33587aee67cd5ac84f2358d7fbd728
libmining <libmining@gmail.com>
2015-05-27 17:01:40 +0900

    add comment

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-
-  res.render('index',
-    {
-      sender: req.query.sender,
-      room: req.query.room
-    }
-  );
-});
 
 
 router.get('/:room', function(req, res, next) {
@@ -19,7 +9,7 @@ router.get('/:room', function(req, res, next) {
     {
 
       room: req.params.room,
-      me: req.query.me
+      sender: req.query.sender
     }
   );
 });
